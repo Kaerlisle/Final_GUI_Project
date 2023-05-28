e265cffa36403ed84002a5afb9b7478b8d8cf030
Kaerlisle <francisgohan@gmail.com>
2023-05-28 22:43:14 +0800

    Commit

--- src/Class2.java
+++ src/Class2.java
@@ -1,9 +1,2 @@
 public class Class2 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello Daryl World");
-        }
-    }
 }
