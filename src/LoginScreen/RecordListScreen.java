f185dcf9f647ca6c9f75776baf31e276a57488f5
claire <maclaire.solis.cics@ust.edu.ph>
2023-05-30 10:10:57 +0800

    test

--- src/LoginScreen/RecordListScreen.java
+++ src/LoginScreen/RecordListScreen.java
@@ -20,20 +20,16 @@ public class RecordListScreen {
         frame.setVisible(true);
         frame.setLayout(new GridLayout(4, 4));
 
-        //list of records of user's name, birthday, and age
-        JPanel titleOfRecords = new JPanel(new GridLayout(1, 3));
-        JLabel name = new JLabel("Name");
-        JLabel bday = new JLabel("Birthday");
-        JLabel age = new JLabel("Age");
-
-        titleOfRecords.add(name);
-        titleOfRecords.add(bday);
-        titleOfRecords.add(age);
-
         //table for the records of user
         JPanel listsOfRecords = new JPanel(new BorderLayout());
-        JTable records = new JTable();
+        JTable records1 = new JTable();
+
+        String[] titleRecords = {"Name", "Birthday", "Age"};
+        DefaultTableModel records2 = new DefaultTableModel(titleRecords, 0);
+        JTable records = new JTable(records2);
+        JScrollPane scroll = new JScrollPane(records);
         listsOfRecords.add(records);
+
 
         //sorting part where you sort the list in a given order
         JPanel sortingLabels = new JPanel();
@@ -61,8 +57,8 @@ public class RecordListScreen {
         buttons.add(expoB);
 
         //
-        frame.add(titleOfRecords);
-        //frame.add();
+        //frame.add(titleOfRecords);
+        frame.add(listsOfRecords);
         frame.add(sortingLabels);
         frame.add(buttons);
     }
